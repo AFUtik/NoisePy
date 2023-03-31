f6b9bc05f573992bff3a26a436e7d8e5998c1296
AFUtik <coffecraft38@gmail.com>
2023-03-31 22:41:52 +0300

    fixed dot product

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import math
 
 M = 5
 N = 5
-w = 50
-h = 50
-R = np.round(3 * np.random.rand(M, N))
+w = 25
+h = 25
+R = np.round(3 * np.random.rand(N, M))
 V = np.array([[0, 1], [0, -1], [1, 0], [-1, 0]])
 
 class PerlinNoise:
@@ -18,11 +18,11 @@ class PerlinNoise:
     self.yCount = yCount
 
   def f(self, t):
-    return t*t*t * ((6*t-15)*t + 10)
+    return t*t*t*((6*t - 15)*t + 10)
   
   def g(self, a, b, t):
     return a + (b-a) * t
-
+  
   def noise(self):
     xInd = min(math.floor(self.x / w), self.xCount - 1)
     yInd = min(math.floor(self.y / h), self.yCount - 1)
@@ -30,37 +30,36 @@ class PerlinNoise:
       return 0
     if yInd == self.yCount - 1:
       return 0
+    
+    xl = self.x - xInd * w
+    yl = self.y - yInd * h
+    al = self.f(xl / w)
+    be = self.f(yl / h)
 
-    dx = self.x - xInd * w
-    dy = self.y - yInd * h
-
-    al = self.f(dx / w)
-    be = self.f(dy / h)
-    b10 = np.array([dx, dy - h])
-    b00 = np.array([dx, dy])
-    b01 = np.array([dx - w, dy])
-    b11 = np.array([dx - w, dy - h])
+    b10 = np.array([ xl, yl - h ])
+    b11 = np.array([ xl - w, yl - h ])
+    b00 = np.array([ xl, yl ])
+    b01 = np.array([ xl - w, yl ])
 
     e00 = V[int(R[yInd, xInd]), :]
     e01 = V[int(R[yInd, xInd+1]), :]
     e10 = V[int(R[yInd+1, xInd]), :]
     e11 = V[int(R[yInd+1, xInd+1]), :]
-
-    c00 = b00 * e00
-    c01 = b01 * e01
-    c10 = b10 * e10
-    c11 = b11 * e11
-
+    
+    c00 = np.dot(b00, e00)
+    c01 = np.dot(b01, e01)
+    c10 = np.dot(b10, e10)
+    c11 = np.dot(b11, e11)
     cx1 = self.g(c00, c01, al)
     cx2 = self.g(c10, c11, al)
     result = self.g(cx1, cx2, be)
-    return result[1]
+    return result
   
 def main():
   MW = (M - 1) * w
   MH = (N - 1) * h
-  x = np.linspace(0, M-1, MW)
-  y = np.linspace(0, N-1, MH)
+  x = np.linspace(0, 10, MW)
+  y = np.linspace(0, 10, MH)
   XX, YY = np.meshgrid(x, y)
   ZZ = np.zeros((MH, MW))
   for inX in range(MW):
