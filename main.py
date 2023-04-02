ae3c359dad32893530deacdefac1c8986ce089e9
AFUtik <coffecraft38@gmail.com>
2023-04-02 12:59:17 +0300

    deleted class

--- main.py
+++ main.py
@@ -5,55 +5,49 @@ import math
 
 M = 5
 N = 5
-w = 25
-h = 25
+w = 10
+h = 10
 R = np.round(3 * np.random.rand(N, M))
 V = np.array([[0, 1], [0, -1], [1, 0], [-1, 0]])
 
-class PerlinNoise:
-  def __init__(self, x, y, xCount, yCount):
-    self.x = x
-    self.y = y
-    self.xCount = xCount 
-    self.yCount = yCount
-
-  def f(self, t):
-    return t*t*t*((6*t - 15)*t + 10)
+def f(t):
+  return t*t*t*((6*t - 15)*t + 10)
   
-  def g(self, a, b, t):
-    return a + (b-a) * t
+def g(a, b, t):
+  return a + (b-a) * t
   
-  def noise(self):
-    xInd = min(math.floor(self.x / w), self.xCount - 1)
-    yInd = min(math.floor(self.y / h), self.yCount - 1)
-    if xInd == self.xCount - 1:
-      return 0
-    if yInd == self.yCount - 1:
-      return 0
-    
-    xl = self.x - xInd * w
-    yl = self.y - yInd * h
-    al = self.f(xl / w)
-    be = self.f(yl / h)
+def noise(x, y, xCount, yCount):
+  xInd = min(math.floor(x / w), xCount - 1)
+  yInd = min(math.floor(y / h), yCount - 1)
+  if xInd == xCount - 1:
+    return 0
+  if yInd == yCount - 1:
+    return 0
+  
+  xl = x - xInd * w
+  yl = y - yInd * h
+  al = f(xl / w)
+  be = f(yl / h)
 
-    b10 = np.array([ xl, yl - h ])
-    b11 = np.array([ xl - w, yl - h ])
-    b00 = np.array([ xl, yl ])
-    b01 = np.array([ xl - w, yl ])
+  b10 = np.array([ xl, yl - h ])
+  b11 = np.array([ xl - w, yl - h ])
+  b00 = np.array([ xl, yl ])
+  b01 = np.array([ xl - w, yl ])
 
-    e00 = V[int(R[yInd, xInd]), :]
-    e01 = V[int(R[yInd, xInd+1]), :]
-    e10 = V[int(R[yInd+1, xInd]), :]
-    e11 = V[int(R[yInd+1, xInd+1]), :]
-    
-    c00 = np.dot(b00, e00)
-    c01 = np.dot(b01, e01)
-    c10 = np.dot(b10, e10)
-    c11 = np.dot(b11, e11)
-    cx1 = self.g(c00, c01, al)
-    cx2 = self.g(c10, c11, al)
-    result = self.g(cx1, cx2, be)
-    return result
+  e00 = V[int(R[yInd, xInd])]
+  e01 = V[int(R[yInd, xInd+1])]
+  e10 = V[int(R[yInd+1, xInd])]
+  e11 = V[int(R[yInd+1, xInd+1])]
+  
+  c00 = np.dot(b00, e00)
+  c01 = np.dot(b01, e01)
+  c10 = np.dot(b10, e10)
+  c11 = np.dot(b11, e11)
+  
+  cx1 = g(c00, c01, al)
+  cx2 = g(c10, c11, al)
+  result = g(cx1, cx2, be)
+  return result
   
 def main():
   MW = (M - 1) * w
@@ -64,9 +58,7 @@ def main():
   ZZ = np.zeros((MH, MW))
   for inX in range(MW):
     for inY in range(MH):
-        noise1 = PerlinNoise(inY, inX, M, N)
-        noise_value = noise1.noise()
-        ZZ[inY, inX] = noise_value
+        ZZ[inY, inX] = noise(inY, inX, M, N)
   fig = plt.figure(figsize=(8, 8))
   ax = fig.add_subplot(projection="3d")
   ax.plot_surface(YY, XX, ZZ, cmap=cm.inferno)
